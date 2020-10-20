a55ba6392ec80d2c86db1b1416583fd972245359
Matthew <Mattpet26@gmail.com>
2020-10-20 09:42:38 -0700

    working back buttons

--- app/src/main/java/com/petersen/taskmaster/AllTasks.java
+++ app/src/main/java/com/petersen/taskmaster/AllTasks.java
@@ -2,7 +2,11 @@ package com.petersen.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class AllTasks extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class AllTasks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_tasks);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(AllTasks.this, MainActivity.class);
+        AllTasks.this.startActivity(intent);
+        return true;
+    }
+
 }
